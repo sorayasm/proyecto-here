1195902596a031dc52a1a45794ee10f71f9442c0
scarleete <scar.antoine@gmail.com>
2018-10-02 09:30:39 -0300

    componente loguin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { VoluntarioFormComponent } from './componentes/voluntario-form/voluntario-form.component';
 import { HomeComponent } from './componentes/home/home.component';
+import { LoginComponent } from './componentes/login/login.component';
+
+// import de material angular scar
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
     AppComponent,
     VoluntarioFormComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponent,
+    MatButtonModule
   ],
   imports: [
     BrowserModule

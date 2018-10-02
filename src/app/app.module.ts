71b6d2b4d3e450881d66ad34dbb1e983846163f7
scarleete <scar.antoine@gmail.com>
2018-10-02 01:01:35 -0300

    Carpeta para componente home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { VoluntarioFormComponent } from './voluntario-form/voluntario-form.component';
+import { VoluntarioFormComponent } from './componentes/voluntario-form/voluntario-form.component';
+import { HomeComponent } from './componentes/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    VoluntarioFormComponent
+    VoluntarioFormComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule

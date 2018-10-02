e0592daa5d1ddf9a5b841751223cac352e4c5504
CamilaEspinozaMoya <camila.andrea.espinoza@gmail.com>
2018-10-02 09:19:13 -0300

    form voluntarios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { VoluntarioFormComponent } from './voluntario-form/voluntario-form.component';
+import { VoluntarioFormComponent } from './componentes/voluntario-form/voluntario-form.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { VoluntarioFormComponent } from './voluntario-form/voluntario-form.compo
     VoluntarioFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]

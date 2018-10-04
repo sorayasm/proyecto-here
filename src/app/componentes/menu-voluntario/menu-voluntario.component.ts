54e0727b903b199a838594b897c5ec31fb1c99f3
CamilaEspinozaMoya <camila.andrea.espinoza@gmail.com>
2018-10-04 12:13:50 -0300

    asd

--- src/app/componentes/menu-voluntario/menu-voluntario.component.ts
+++ src/app/componentes/menu-voluntario/menu-voluntario.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import {MediaMatcher} from '@angular/cdk/layout';
+import { Component, OnInit, Input } from '@angular/core';
+import { MediaMatcher } from '@angular/cdk/layout';
 import {ChangeDetectorRef, OnDestroy} from '@angular/core';
+import { AngularFireDatabase, AngularFireList } from '@angular/fire/database';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-menu-voluntario',
@@ -8,20 +10,26 @@ import {ChangeDetectorRef, OnDestroy} from '@angular/core';
   styleUrls: ['./menu-voluntario.component.css']
 })
 export class MenuVoluntarioComponent implements OnDestroy {
+  @Input() info;
+  @Input() id;
+  vol$: Observable<any>;
   public query: string;
   mobileQuery: MediaQueryList;
   private _mobileQueryListener: () => void;
 
-  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher, private database: AngularFireDatabase) {
     this.mobileQuery = media.matchMedia('(max-width: 600px)');
     this._mobileQueryListener = () => changeDetectorRef.detectChanges();
     this.mobileQuery.addListener(this._mobileQueryListener);
     this.query = 'Hospital';
+    this.vol$ = this.database.list('/vol').snapshotChanges();
   }
 
   ngOnDestroy(): void {
     this.mobileQuery.removeListener(this._mobileQueryListener);
+
   }
 
 
 }
+

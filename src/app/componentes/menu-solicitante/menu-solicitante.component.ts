2a011e00879b4b8c9792766fb544ed5a392a1d8f
CamilaEspinozaMoya <camila.andrea.espinoza@gmail.com>
2018-10-04 09:59:18 -0300

    lista estructura menu

--- src/app/componentes/menu-solicitante/menu-solicitante.component.ts
+++ src/app/componentes/menu-solicitante/menu-solicitante.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import {MediaMatcher} from '@angular/cdk/layout';
+import {ChangeDetectorRef, OnDestroy} from '@angular/core';
 
 @Component({
   selector: 'app-menu-solicitante',
   templateUrl: './menu-solicitante.component.html',
   styleUrls: ['./menu-solicitante.component.css']
 })
-export class MenuSolicitanteComponent implements OnInit {
+export class MenuSolicitanteComponent implements OnDestroy {
+  public query: string;
+  mobileQuery: MediaQueryList;
+  private _mobileQueryListener: () => void;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
+    this.query = 'Hospital';
   }
+
+  ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this._mobileQueryListener);
+  }
+
 
 }
--- src/app/componentes/menu-voluntario/menu-voluntario.component.ts
+++ src/app/componentes/menu-voluntario/menu-voluntario.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import {MediaMatcher} from '@angular/cdk/layout';
+import {ChangeDetectorRef, OnDestroy} from '@angular/core';
 
 @Component({
   selector: 'app-menu-voluntario',
   templateUrl: './menu-voluntario.component.html',
   styleUrls: ['./menu-voluntario.component.css']
 })
-export class MenuVoluntarioComponent implements OnInit {
+export class MenuVoluntarioComponent implements OnDestroy {
+  public query: string;
+  mobileQuery: MediaQueryList;
+  private _mobileQueryListener: () => void;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
+    this.query = 'Hospital';
   }
+
+  ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this._mobileQueryListener);
+  }
+
 
 }
